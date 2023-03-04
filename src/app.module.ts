ce1386ee64b5d1421b52a4dc212f964011ccda46
Albescu Cristian <albescucristian@Albescus-MacBook-Air.local>
2023-03-04 17:43:45 +0200

    feat: add config package and .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [appConfig] })],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/app.config.ts
@@ -0,0 +1,5 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('app', () => ({
+  port: parseInt(process.env.APP_BASE_URL) || 8080,
+}));
